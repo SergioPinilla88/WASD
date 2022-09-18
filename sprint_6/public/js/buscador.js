baabab4c56c1ae7465e5b41a33d381f9e44ff525
Nahuel Galarza <ezequieltdj@gmail.com>
2022-09-18 15:04:43 -0300

    BuscadorDeGit

---
+++ sprint_6/public/js/buscador.js
@@ -0,0 +1,63 @@
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggbox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let linkTag = searchWrapper.querySelector("a");
+let webLink;
+
+inputBox.onkeyup =(e)=>{
+    let userData= e.target.value;
+    let emptyArray = [];
+    if(userData){
+        icon.onclick = () => {
+            webLink = `https://www.google.es/search?q=${userData}`;
+            linkTag.setAttribute("href",webLink);
+            linkTag.click();
+        }
+
+        emptyArray = suggestions.filter((data)=>{
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+
+        emptyArray = emptyArray.map((data)=>{
+            return data =  `<li onclick = "select(event)">${data}</li>`;
+        });
+
+        searchWrapper.classList.add("active");
+        showSuggestions(emptyArray);
+        let allList = suggbox.querySelectorAll("li");
+
+        for (let i = 0; i< allList.length; i++) {
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active");
+    }
+
+}
+
+
+
+
+function select(element){
+    let selectData = element.textContent;
+    inputBox.value = selectData;
+    icon.onclick = ()=>{
+        webLink = `https://www.google.es/search?q=${selectData}`;
+        linkTag.setAttribute("href",webLink);
+        linkTag.click();
+    }
+    searchWrapper.classList.remove("active");
+
+}
+
+function showSuggestions(list){
+    let listData;
+    if (!list.length) {
+        userValue=inputBox.value;
+        listData=`<li>${data}</li>`;
+    }else{
+        listData=list.join("");
+    }
+    suggbox.innerHTML=listData;
+}
---
+++ sprint_6/public/js/suggestions.js
@@ -0,0 +1,6 @@
+let suggestions = [
+    "Far Cry 6 PS5",
+    "Control PS5",
+    
+
+];
