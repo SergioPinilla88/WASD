f0cdf70bb0d309d3aa98007c2159d78d1399dd35
Nahuel Galarza <ezequieltdj@gmail.com>
2022-09-23 19:39:58 -0300

    ProductosValidacion

--- sprint_7/public/js/productos.js
+++ sprint_7/public/js/productos.js
@@ -1,26 +1,29 @@
 window.addEventListener('load', function() {
     let button = document.querySelector('form.productos');
-    button.addEventListener('submit', function(evento) {
-  
-      evento.preventDefault();
+    button.addEventListener('submit', function(e) {
+
       let nombre = document.querySelector("#name");
-      if(nombre.value = ""){
-        console.log('Hubo un error en el nombre');
+      if(nombre.value.length > 3 ){
+        
       }else if(nombre.value.length < 3){
-        alert('No funciona');
+        e.preventDefault();
       };
+
       let descripcion = document.querySelector("#descripcion");
       if( descripcion.value.length < 4){
         console.log('Hubo un error en el descripcion');
       };
+
       let fabricante = document.querySelector("#fabricante");
       if( fabricante.value.length < 4){
         console.log('Hubo un error en el fabricante');
       };
+      
       let paisOrigen = document.querySelector("#paisOrigen");
       if( paisOrigen.value.length < 4){
         console.log('Hubo un error en el paisOrigen');
       };
+
       let modelo = document.querySelector("#modelo");
       if( modelo.value.length < 4){
         console.log('Hubo un error en el modelo');
