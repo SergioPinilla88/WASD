90e657334e1b05ecef57e382b45188e080ffa68b
RodrigoArcibia <ac_rodrigo_1990@hotmail.com>
2022-09-24 00:29:57 -0300

    Validacion Front

--- sprint_7/database/config/config.js
+++ sprint_7/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
-    "username": "akepes",
-    "password": "Alejo*20220625",
+    "username": "root",
+    "password": "",
     "database": "wasd",
     "host": "127.0.0.1",
     "dialect": "mysql"
